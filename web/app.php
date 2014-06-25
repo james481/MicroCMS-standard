791f1f9628a4f5c4852faeff64d7b67d79929e8f
James Watts <jamescwatts@gmail.com>
2014-06-25 12:54:50 -0400

    Rootdir update / .gitkeeps

--- web/app.php
+++ web/app.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 $kernel = new Kernel('prod');
+$kernel->setRootDir(__DIR__ . '/../');
 $req = Request::createFromGlobals();
 $res = $kernel->handle($req);
 $res->send();
