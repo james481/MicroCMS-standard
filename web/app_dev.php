23afe80c0185a4f947484304ba850c61fed5816f
James Watts <jamescwatts@gmail.com>
2014-06-25 12:16:11 -0400

    Initial commit of application package

---
+++ web/app_dev.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+* web/app.php
+*
+* Main Application Entry (development)
+*
+* @author James Watts <jamescwatts[at]gmail[dot]com>
+*
+*/
+
+use MicroCMS\Kernel\Kernel;
+use Symfony\Component\HttpFoundation\Request;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$kernel = new Kernel('dev');
+$req = Request::createFromGlobals();
+$res = $kernel->handle($req);
+$res->send();
+$kernel->terminate();
